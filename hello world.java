c7f17b626296bd7a14b463b39da60ac6dd349ecb
gshreyanreddy <137819800+gshreyanreddy@users.noreply.github.com>
2023-07-22 19:00:41 +0000

    hello world.java

---
+++ hello world.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int x=s.nextInt();
+        System.out.println(x+"Hello World");
+    }
+}
